571ac38bf2bb79e51a3da52c4788d3d26a537edd
Alex Neumark <alexneumark@Alexs-MacBook-Air.local>
2025-12-19 01:38:38 +0300

    some improvments

--- taxi_project/wsgi.py
+++ taxi_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Kursach project.
+WSGI config for taxi_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kursach.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi_project.settings')
 
 application = get_wsgi_application()
